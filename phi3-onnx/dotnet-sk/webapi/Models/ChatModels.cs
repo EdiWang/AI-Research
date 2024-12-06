0161969e5cae531e465b9b7bb124361aaeeda10a
Edi Wang <Edi.Wang@outlook.com>
2024-12-06 09:20:38 +0800

    rename project

---
+++ phi3-onnx/dotnet-sk/webapi/Models/ChatModels.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Edi.Phi3SkOnnxApi.Models;
+
+public class ChatRequest
+{
+    [Required]
+    public Message[] Messages { get; set; }
+}
+
+public class Message
+{
+    public string Role { get; set; } = "user";
+
+    [Required]
+    public string Content { get; set; }
+}
